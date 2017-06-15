9897f752659fc6eadf6d57e554b95f861ea1a1d3
Frank Corso <frank@mylocalwebstop.com>
2017-06-14 23:10:55 -0400

    Creates quote model

---
+++ models.py
@@ -0,0 +1,26 @@
+from peewee import *
+
+import config
+
+
+class Quote(Model):
+	"""The model for our quotes table. Includes columns for quote and author."""
+	quote = TextField(unique=True)
+	author = TextField()
+
+	@staticmethod
+	def random_quote():
+		"""Returns a random quote from database"""
+		try:
+			return Quote.select().order_by(fn.Random()).get()
+		except DoesNotExist:
+			return None
+
+	class Meta:
+		database = config.DATABASE
+
+def initialize():
+	"""Creates the tables in not already created"""
+	config.DATABASE.connect()
+	config.DATABASE.create_tables([Quote], safe=True)
+	config.DATABASE.close()
