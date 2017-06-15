f6980c4719c98b90abb5372b8c959fa5a48f92eb
Frank Corso <frank@mylocalwebstop.com>
2017-06-14 23:50:45 -0400

    Creates form to submit quotes

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class AddQuoteForm(Form):
+	quote = StringField(
+		'quote',
+		validators=[DataRequired()]
+	)
+	author = StringField(
+		'author',
+		validators=[DataRequired()]
+	)
