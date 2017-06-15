4c1bba1419c6a15136576a2e253f4dc27adf092b
Frank Corso <frank@mylocalwebstop.com>
2017-06-14 23:50:37 -0400

    Creates flask app to add quotes to db

---
+++ add_quote.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, url_for, flash
+
+import models
+import forms
+import config
+
+
+app = Flask(__name__)
+app.secret_key = config.SECRET_KEY
+
+
+@app.route('/', methods=('GET', 'POST'))
+def add_quote():
+	form = forms.AddQuoteForm()
+	if form.validate_on_submit():
+		flash("New quote added!")
+		models.Quote.create_quote(form.quote.data, form.author.data)
+	return render_template('add_quote.html', form=form)
+
+if __name__ == '__main__':
+	models.initialize()
+	app.run(debug=config.DEBUG, host=config.HOST, port=config.PORT)
