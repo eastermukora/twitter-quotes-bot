b73440294c5c8d03ab3dd264ff0a07576815bf84
Frank Corso <frank@mylocalwebstop.com>
2017-08-15 16:18:46 -0400

    Adds name and username to return of photo functions

--- photos.py
+++ photos.py
@@ -17,20 +17,20 @@ def get_random_photo():
 		orientation='landscape'
 	)
 	if photo:
-		return photo[0]["urls"]["full"]
+		return {'url': photo[0]["urls"]["full"], 'name': photo[0]["user"]["name"], 'user': photo[0]["user"]["username"]}
 	return None
 
 
 def create_photo(filename):
 	"""Creates a photo by downloading it from Unsplash URL"""
-	photo_url = get_random_photo()
-	if photo_url:
+	photo = get_random_photo()
+	if photo:
 		f = open(filename, 'wb')
 		f.write(
 			requests.get(
-				photo_url
+				photo["url"]
 			).content
 		)
 		f.close()
-		return filename
+		return photo
 	return None
