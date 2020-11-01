5bf3a325b332683422b0b7bcc55e698d6d34896d
Frank Corso <frank@frankcorso.me>
2020-11-01 12:30:36 -0500

    Make quotes get appended to file, not overwrite

--- quotes.py
+++ quotes.py
@@ -1,13 +1,27 @@
 import csv
+import random
 
 
 def load_quotes():
     """Loads all quotes from the CSV."""
-    pass
+    quotes = []
+    with open('quotes.csv', newline='') as quotes_file:
+        csv_reader = csv.reader(quotes_file)
+        for row in csv_reader:
+            if len(row) != 0:
+                quotes.append({'quote': row[0], 'author': row[1]})
+    return quotes
 
 
 def get_random_quote():
     """Returns a random quote."""
-    quote = ''
-    author = ''
-    return quote, author
+    quotes = load_quotes()
+    value = random.choice(quotes)
+    return value['quote'], value['author']
+
+
+def add_quote(quote, author):
+    """Adds a quote to the CSV."""
+    with open('quotes.csv', mode="a") as quotes_file:
+        csv_writer = csv.writer(quotes_file)
+        csv_writer.writerow([quote, author])
