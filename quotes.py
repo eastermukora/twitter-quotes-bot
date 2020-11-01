f883fc3d49402e961ba54ea671b2b715052e053c
Frank Corso <frank@frankcorso.me>
2020-11-01 12:11:47 -0500

    Add new file for loading and getting quotes

---
+++ quotes.py
@@ -0,0 +1,13 @@
+import csv
+
+
+def load_quotes():
+    """Loads all quotes from the CSV."""
+    pass
+
+
+def get_random_quote():
+    """Returns a random quote."""
+    quote = ''
+    author = ''
+    return quote, author
